131c33eaf893a8ba355b88cff24b6bbaaab87e68
Juan Galarce <j6alarce@gmail.com>
2021-01-18 18:30:37 -0300

    Leyendo el presupuesto del usuario y guardandolo en el state

--- src/componentes/Pregunta.js
+++ src/componentes/Pregunta.js
@@ -1,15 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Pregunta = () => {
+
+    // Definit state
+    const [cantidad, setCantidad] = useState(0);
+
+
+    // Funcion que lee el presupuesto
+    const definitPresupuesto = e => {
+        setCantidad( parseInt(e.target.value, 10))
+    }
+
+    // Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+
+        // Validar
+
+
+        // Si pasa la validacion.
+    }
+
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
 
-      <form>
+      <form
+        onSubmit={agregarPresupuesto}
+      >
         <input
           type="number"
           className="u-full-width"
           placeholder="Coloca tu Presupuesto."
+          onChange={definitPresupuesto}
         />
 
         <input
