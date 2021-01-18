2b4d90319a5d8b87104b0cd1373ee46ecbdbf857
Juan Galarce <j6alarce@gmail.com>
2021-01-18 20:21:46 -0300

    Creando formulario de gasto y state para ocultar el presupuesto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Pregunta from "./componentes/Pregunta";
+import Formulario from "./componentes/Formulario";
 
 function App() {
   // definir el state
   const [presupuesto, setPresupuesto] = useState(0);
   const [restante, setRestante] = useState(0);
+  const [mostrarPregunta, setMostrarPregunta] = useState(true);
 
   return (
     <div className="container">
@@ -12,7 +14,20 @@ function App() {
         <h1>Gasto Semanal</h1>
 
         <div className="contenido-principal contenido">
-          <Pregunta setPresupuesto={setPresupuesto} setRestante={setRestante} />
+          {mostrarPregunta ? (
+            <Pregunta
+              setPresupuesto={setPresupuesto}
+              setRestante={setRestante}
+              setMostrarPregunta={setMostrarPregunta}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario />
+              </div>
+              <div className="one-half column">2</div>
+            </div>
+          )}
         </div>
       </header>
     </div>
--- src/componentes/Pregunta.js
+++ src/componentes/Pregunta.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from "react";
 import Error from './Error';
 
-const Pregunta = ({ setPresupuesto, setRestante }) => {
+const Pregunta = ({ setPresupuesto, setRestante, setMostrarPregunta }) => {
 
     // Definit state
     const [cantidad, setCantidad] = useState(0);
@@ -27,6 +27,7 @@ const Pregunta = ({ setPresupuesto, setRestante }) => {
         setError(false);
         setPresupuesto(cantidad);
         setRestante(cantidad);
+        setMostrarPregunta(false)
     }
 
 
